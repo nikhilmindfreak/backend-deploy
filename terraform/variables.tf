666e4ab914eed9c79bfdf6849f2a2121609d72ba
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-07-02 19:42:26 -0400

    Initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Environment = "dev"
+    Terraform = "true"
+    Component = "backend"  #for billing purpose we give mandotry tag, #extra tag
+  }
+}
+
+variable "zone_name" {
+  default = "devopsme.online"
+}
+
+variable "app_version" {  # we added version now with nexus and vesrion control
+ 
+}
