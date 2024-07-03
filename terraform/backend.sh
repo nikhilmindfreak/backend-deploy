4523c562a294daf0fc475cccc91412c2a242f694
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-07-03 18:06:05 -0400

    Initial commit

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 component=$1
 environment=$2
-appVersion=$3
+app_Version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/nikhilmindfreak/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$appVersion
+ansible-pull -i localhost, -U https://github.com/nikhilmindfreak/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version 
